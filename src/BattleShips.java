2bde5bdea3dd1d5d1cbb3d9ec7d5643a899f854a
Cullen, Luke William <lukewcullen@gmail.com>
2018-06-07 14:21:17 +1200

    Test file added

--- src/BattleShips.java
+++ src/BattleShips.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class BattleShips {
     public static void main(String[] args)
     {
@@ -11,6 +14,17 @@ public class BattleShips {
             String file = args[0];
             int attempts = Integer.parseInt(args[1]);
             System.out.println("File: " + file + " attempts: " + attempts);
+
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+
+            while((line = br.readLine())!= null)
+            {
+                System.out.println(line);
+            }
+
         }
         catch(Exception e)
         {
